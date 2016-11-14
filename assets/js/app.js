7472e1f5bcf767c0af3943db878ed8ad957eae6a
Walter Mattingly <waltdakind@gmail.com>
2016-11-14 17:46:28 -0500

    basic files, minimal styling

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+
+
+//api route for the quote 
+var api = 'http://quotes.stormconsultancy.co.uk/random.json';
+//the ajax call function
+function newQuote(){
+$.get( api, function( data ) {
+	console.log(data.quote);
+$('#randomQuote').html(data.quote);	
+    console.log(data.author);
+$('#randomAuthor').html(data.author);
+});	
+}
+
+
+//on doc ready have a quote/author loaded 
+$(function(){
+
+   //load initial quote
+newQuote();
+//on click make ajax call for a new quote/author
+$( "button" ).click(function() {
+  console.log('button clicked');
+ newQuote(); 
+});
+});
+
