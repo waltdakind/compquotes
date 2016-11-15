27cf29711f1c912abcecb3f9e8c858c880600329
Walter Mattingly <waltdakind@gmail.com>
2016-11-15 16:12:15 -0500

    tweet button populates with quote

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,19 @@ $.get( api, function( data ) {
 $('#randomQuote').html(data.quote);	
     console.log(data.author);
 $('#randomAuthor').html(data.author);
+twttr.widgets.createShareButton(
+  '/',
+  document.getElementById('quoteTweet'),
+  {
+    text: '"' + data.quote + '"' + ' - ' + data.author
+  }
+);
+
+
 });	
 }
 
-
+//https://twitter.com/intent/tweet?hashtags=quotes%2C%20programming&ref_src=twsrc%5Etfw&related=twitterapi%2Ctwitter&text=Why%20you%20no%20update%3F&tw_p=tweetbutton&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&via=twitterdev
 //on doc ready have a quote/author loaded 
 $(function(){
 
@@ -21,6 +30,7 @@ newQuote();
 //on click make ajax call for a new quote/author
 $( "button" ).click(function() {
   console.log('button clicked');
+  $('#quoteTweet').empty();
  newQuote(); 
 });
 });
