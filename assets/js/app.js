e4c7811b53da3c3c047b16d4c357f2957451136e
Walter Mattingly <waltdakind@gmail.com>
2017-01-08 02:17:56 -0500

    cross domain bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,14 +20,12 @@ twttr.widgets.createShareButton(
     text: '"' + data.quote + '"' + ' - ' + data.author
   }
 );
-
-
+//end tweet quote
 }
+//end ajax
 });
 }
 
-//https://twitter.com/intent/tweet?hashtags=quotes%2C%20programming&ref_src=twsrc%5Etfw&related=twitterapi%2Ctwitter&text=Why%20you%20no%20update%3F&tw_p=tweetbutton&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&via=twitterdev
-//on doc ready have a quote/author loaded 
 $(function(){
 
    //load initial quote
