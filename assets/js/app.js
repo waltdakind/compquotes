105ad0a53f5bd273e00ae5701195c3e386bfab53
Walter Mattingly <waltdakind@gmail.com>
2017-01-08 02:11:28 -0500

    crossdomain bug fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 
 
 //api route for the quote 
-var api = 'https://quotes.stormconsultancy.co.uk/random.json/';
+var api = 'http://quotes.stormconsultancy.co.uk/random.json/';
 //the ajax call function
 function newQuote(){
-$.get( api, function( data ) {
-	console.log(data.quote);
-$('#randomQuote').html(data.quote);	
+$.ajax({
+  url: api,
+  dataType: "jsonp",
+  crossDomain: true,
+  success: function (data) {
+  console.log(data.quote);
+$('#randomQuote').html(data.quote); 
     console.log(data.author);
 $('#randomAuthor').html(data.author);
 twttr.widgets.createShareButton(
@@ -18,7 +22,8 @@ twttr.widgets.createShareButton(
 );
 
 
-});	
+}
+});
 }
 
 //https://twitter.com/intent/tweet?hashtags=quotes%2C%20programming&ref_src=twsrc%5Etfw&related=twitterapi%2Ctwitter&text=Why%20you%20no%20update%3F&tw_p=tweetbutton&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&via=twitterdev
